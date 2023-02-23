a390845c9823199512057d588993acf76fa0d368
Jayant Verma <98758438+jayantverma2809@users.noreply.github.com>
2023-02-24 05:13:16 +0530

    Add files via upload

---
+++ Prediction.py
@@ -0,0 +1,94 @@
+import pandas as pd
+import re
+import nltk
+import emoji
+import string
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+import pickle
+
+def strip_emoji(text):
+    return emoji.replace_emoji(text,replace="")
+
+def strip_all_entities(text):
+    stop_words = set(stopwords.words('english')) 
+    text = text.replace('\r', '').replace('\n', ' ').lower()
+    text = re.sub(r"(?:\@|https?\://)\S+", "", text)
+    text = re.sub(r'[^\x00-\x7f]',r'', text)
+    text = re.sub(r'(.)1+', r'1', text)
+    text = re.sub('[0-9]+', '', text)
+    stopchars= string.punctuation
+    table = str.maketrans('', '', stopchars)
+    text = text.translate(table)
+    text = [word for word in text.split() if word not in stop_words]
+    text = ' '.join(text)
+    return text
+
+def decontract(text):
+    text = re.sub(r"can\'t", "can not", text)
+    text = re.sub(r"n\'t", " not", text)
+    text = re.sub(r"\'re", " are", text)
+    text = re.sub(r"\'s", " is", text)
+    text = re.sub(r"\'d", " would", text)
+    text = re.sub(r"\'ll", " will", text)
+    text = re.sub(r"\'t", " not", text)
+    text = re.sub(r"\'ve", " have", text)
+    text = re.sub(r"\'m", " am", text)
+    return text
+
+def clean_hashtags(text):
+    tweet = " ".join(word.strip() for word in re.split('#(?!(?:hashtag)\b)[\w-]+(?=(?:\s+#[\w-]+)*\s*$)', text))
+    clean_tweet = " ".join(word.strip() for word in re.split('#|_', tweet))
+    return clean_tweet
+
+def filter_chars(text):
+    sent = []
+    for word in text.split(' '):
+        if ('$' in word) | ('&' in word):
+            sent.append('')
+        else:
+            sent.append(word)
+    return ' '.join(sent)
+
+def remove_mult_spaces(text):
+    return re.sub("\s\s+" , " ", text)
+
+def stemmer(text):
+    tokenized = nltk.word_tokenize(text)
+    ps = PorterStemmer()
+    return ' '.join([ps.stem(words) for words in tokenized])
+
+def lemmatize(text):
+    tokenized = nltk.word_tokenize(text)
+    lm = WordNetLemmatizer()
+    return ' '.join([lm.lemmatize(words) for words in tokenized])
+
+def preprocess(text):
+    text = strip_emoji(text)
+    text = decontract(text)
+    text = strip_all_entities(text)
+    text = clean_hashtags(text)
+    text = filter_chars(text)
+    text = remove_mult_spaces(text)
+    text = stemmer(text)
+    text = lemmatize(text)
+    return text
+
+def prediction(text):
+    text = pd.Series(text)
+    text = preprocess(text)
+    text = pd.Series(text)
+    vectoriser = pickle.load(open("vectorizer.pkl", "rb"))
+    text = vectoriser.transform(text)
+    model = pickle.load(open("model.pkl", "rb"))
+    prediction = model.predict(text)
+    prediction = prediction[0]
+
+    sentiments = {1:"religion",2: "age",3: "ethnicity",4: "gender",
+                        5 : "other_cyberbullying",6: "not_cyberbullying"}
+
+    for i in sentiments.keys():
+        if i == prediction:
+            return sentiments[i]
---
+++ app.py
@@ -0,0 +1,111 @@
+import pandas as pd
+import streamlit as st
+from PIL import Image
+import pandas as pd
+import numpy as np
+import re
+import string
+from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk import PorterStemmer, WordNetLemmatizer
+from Prediction import *
+import pickle
+
+# Page title
+st.set_page_config(page_title="Jayant Verma", page_icon=Image.open('statics/me.png'))
+
+# Add profile image
+profile_image = Image.open("statics/me.png")
+st.sidebar.image(profile_image, use_column_width=True)
+
+# Add contact information
+st.sidebar.title("Jayant Verma")
+st.sidebar.write("Data Scientist")
+st.sidebar.write("You can reach me at:")
+st.sidebar.subheader("jayantverma9380@gmail.com")
+st.sidebar.subheader("[LinkedIn](https://www.linkedin.com/in/jayantverma28)")
+
+# GitHub
+st.sidebar.subheader("[GitHub](https://github.com/jayantverma2809)")
+
+#Skills
+st.sidebar.header("Skills")
+st.sidebar.write("Here are some of my top skills:")
+st.sidebar.write("- Python programming")
+st.sidebar.write("- SQL")
+st.sidebar.write("- Data analysis and visualization")
+st.sidebar.write("- Machine learning")
+st.sidebar.write("- NLP")
+
+#Projects
+st.sidebar.title("Other Projects")
+st.sidebar.write("Here are some of my projects:")
+st.sidebar.header("Machine Learning Projects")
+st.sidebar.subheader("[Used Phone Price Prediction](https://usedphonepriceprediction.azurewebsites.net/)")
+st.sidebar.write("Description: Using unsupervised learning techniques to predict prices of used phones using their various features such as days used, camera, battery,etc.")
+st.sidebar.header("Data Preprocessing Projects")
+st.sidebar.subheader("[EDA & Feature Engineering - Bike Sharing Data](https://lnkd.in/dzjAsajs)")
+st.sidebar.write("Description: Under this data preprocessing project, I have performed time series analysis, exploratory data analysis and various feature engineering techniques such as transformations, handling outliers, etc to convert raw data into model training ready data.")
+st.sidebar.subheader("[EDA & Feature Engineering - Wine Quality Data](https://lnkd.in/dKRMT7Ym)")
+st.sidebar.write("Under this data preprocessing project, I have performed exploratory data analysis and various feature engineering techniques such as transformations, handling outliers, standardization to convert raw data into model training ready data.")
+
+# Main Page Starts Here !
+st.write('''
+# Cyberbullying Tweet Recognition App
+
+This app predicts the nature of the tweet into 6 Categories.
+* Age
+* Ethnicity
+* Gender
+* Religion
+* Other Cyberbullying
+* Not Cyberbullying
+
+***
+''')
+
+image = Image.open('statics/twitter.png')
+st.image(image, use_column_width= True)
+
+# Text Box
+st.header('Enter Tweet ')
+tweet_input = st.text_area("Tweet Input", height= 150)
+print(tweet_input)
+st.write('''
+***
+''')
+
+# print input on webpage
+if tweet_input:
+    st.header('''
+    ***Predicting......
+    ''')
+else:
+    st.write('''
+    ***No Tweet Text Entered!***
+    ''')
+st.write('''
+***
+''')
+
+# Output on the page
+st.header("Prediction")
+if tweet_input:
+    prediction = prediction(tweet_input)
+    if prediction == "age":
+        st.image("statics/Age.png",use_column_width= True)
+    elif prediction == "ethnicity":
+        st.image("statics/Ethnicity.png",use_column_width= True)
+    elif prediction == "gender":
+        st.image("statics/Gender.png",use_column_width= True)
+    elif prediction == "other_cyberbullying":
+        st.image("statics/Other.png",use_column_width= True)
+    elif prediction == "religion":
+        st.image("statics/Religion.png",use_column_width= True)
+    elif prediction == "not_cyberbullying":
+        st.image("statics/not_cyber.png",use_column_width= True)
+else:
+    st.write('''
+    ***No Text Entered!***
+    ''')
+
+st.write('''***''')
